dd5debd2f1ce405bae0f0dab812f5e2fa5871648
Abdulhay <abdulhayuralov8185603@gmail.com>
2022-08-22 02:37:45 +0500

    time

--- src/main/java/uz/process/email/controller/SenderController.java
+++ src/main/java/uz/process/email/controller/SenderController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import uz.process.email.payload.MailPayload;
 import uz.process.email.service.EmailService;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/mail")
@@ -14,12 +16,12 @@ public class SenderController {
 
     @GetMapping("send/{text}")
     public String sendEmail(@PathVariable String text) {
-        return service.sendSimpleMessage("abdulhay.uralov@yahoo.com", "sending with java, method get", text);
+        return service.sendSimpleMessage("abdulhay.uralov@yahoo.com", "sending with java, method get", text+ "\n" + LocalDateTime.now().toString());
     }
 
     @PostMapping("/send")
     public String sendToGmail(@RequestBody MailPayload payload) {
         System.out.println(payload);
-        return service.sendSimpleMessage(payload.getGmail(), "sending with java", payload.getText());
+        return service.sendSimpleMessage(payload.getGmail(), "sending with java", payload.getText() + "\n" + LocalDateTime.now().toString());
     }
 }
